d9fdc2d2f8a6e882e1ddebb29484f1fca529fe8a
Matthew King <king.matthias@gmail.com>
2024-12-12 14:55:11 -0500

    update searchLocataion()

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,6 +1,12 @@
-const teamMembers = document.getElementById('ourTeam');
-teamMembers.innerHTML = "";
+const searchBtn = document.getElementById('searchBtn');
+const clearBtn = document.getElementById('clearBtn');
+
+
+
+
 function listTeam() {
+    const teamMembers = document.getElementById('ourTeam');
+    teamMembers.innerHTML = "";
     fetch('travel_team.json')
         .then(response => response.json())
         .then(data => {
@@ -13,3 +19,28 @@ function listTeam() {
             teamMembers.innerHTML = 'An error occurred while fethcin data';
         });
 }
+
+function searchLocations() {
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    const rightDiv = document.getElementById('rightSide');
+    rightDiv.innerHTML = "";
+
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            const keyword = data.find(item => item === input);
+            
+            if (keyword) {
+                rightDiv.innerHTML += `<ul><li><h2>${keyword.id[0].name}</h2>`;
+                rightDiv.innerHTML += `</li></ul>`;
+            } else {
+                rightDiv.innerHTML = `${keyword} not found.`;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            rightDiv.innerHTML = 'An error occurred while fetching data.';
+        });
+}
+
+searchBtn.addEventListener('click', searchLocations);
